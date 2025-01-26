22fee212119ccabe9111957b717b680ef98a63f8
Александра <nekrasovasasha7@mail.ru>
2025-01-26 21:54:28 +0300

    first commit

---
+++ src/main/java/com/mycompany/client_for_sftp/Client_for_SFTP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.client_for_sftp;
+
+/**
+ *
+ * @author Александра
+ */
+public class Client_for_SFTP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
